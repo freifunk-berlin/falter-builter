3360b1192db7acd89ddc8f8f0895f17db52c8578
pmelange <isprotejesvalkata@gmail.com>
2020-12-08 13:11:38 +0100

    footer: optimize script to pull the currect luci footer
    Using the stored value of FREIFUNK_OPENWRT_BASE, the correct
    footer.htm file will be downloaded.  FREIFUNK_OPENWRT_BASE is
    found in the freifunk_release file in the falter-common package.
    
    Signed-off-by: pmelange <isprotejesvalkata@gmail.com>

--- scripts/03-luci-footer.sh
+++ scripts/03-luci-footer.sh
@@ -2,10 +2,11 @@ VERSION="$1"
 NICKNAME="$2"
 TARGET="$3"
 SUBTARGET="$4"
+OPENWRT_BASE="$5"
 # get current path of script. Thus we can call the script from everywhere.
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
-SRCFOOTER="https://raw.githubusercontent.com/openwrt/luci/master/themes/luci-theme-bootstrap/luasrc/view/themes/bootstrap/footer.htm"
+SRCFOOTER="https://raw.githubusercontent.com/openwrt/luci/${OPENWRT_BASE}/themes/luci-theme-bootstrap/luasrc/view/themes/bootstrap/footer.htm"
 FOOTERDIR="$SCRIPTPATH/../embedded-files/usr/lib/lua/luci/view/themes/bootstrap/"
 mkdir -p $FOOTERDIR
 
