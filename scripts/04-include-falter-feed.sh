3a5aa6129171581691f01bce9fd4d6b911aafa92
Martin Hübner <martin.hubner@web.de>
2021-01-18 00:08:16 +0100

    build_falter: include falter-feed into images
    The imagsbuilder doesn't include the build-time falter-feed into
    the images by itself. This commit enables that, by generating a
    customfeeds.conf-file for opkg and include it into the images via
    the embedded_files directory. Adds also the feed-key.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

---
+++ scripts/04-include-falter-feed.sh
@@ -0,0 +1,20 @@
+# inject the feed line into the file customfeed.conf and
+# include that file via embedded-files into images.
+
+# get current path of script. Thus we can call the script from everywhere.
+SCRIPTPATH=$(dirname $(readlink -f "$0"))
+
+mkdir -p "$SCRIPTPATH/../embedded-files/etc/opkg/keys"
+
+# load package-key and post it to dir. keyname is keys fingerprint.
+URL="https://buildbot.berlin.freifunk.net/buildbot/feed/packagefeed_master.pub"
+curl "$URL" > "$SCRIPTPATH/../embedded-files/etc/opkg/keys/61a078a38408e710"
+
+# We inherited $FALTER_REPO_BASE from caller whom exported it.
+printf \
+"
+# add your custom package feeds here
+#
+# src/gz example_feed_name http://www.example.com/path/to/files
+$FALTER_REPO_BASE
+" > "$SCRIPTPATH/../embedded-files/etc/opkg/customfeeds.conf"
