8de2fb1468d5fcdf63c7f03d002d121d79751cf1
Packet Please <pktpls@systemli.org>
2024-05-30 03:32:22 +0200

    vm: rework, including post-wizard reboot

--- test/vm.sh
+++ test/vm.sh
@@ -2,42 +2,51 @@
 
 # shellcheck disable=SC2015
 
-function usage(){
-  echo
-  echo "Usage: ./vm.sh <hostname>"
-  echo
-  exit 1
+function usage() {
+    local v="$1"
+    local versions
+
+    echo "usage: test/vm.sh <version>"
+    echo
+    echo "available falter versions:"
+    versions=$(ls out/*/"$v"/x86/64/*-generic-kernel.bin 2>/dev/null)
+    if [ -n "$versions" ]; then
+        echo $versions | xargs -n1 | cut -d'/' -f 2 | xargs -n1 echo -n " "
+        echo
+    else
+        echo "  n/a"
+    fi
+    echo
+    echo "hint: use build/build.sh to build x86/64 images"
+    echo
+    exit 1
 }
 
+set -o pipefail
 set -e
-[ -n "$TRACE_VMSH" ] && set -x || true
+set -x
 
-[ -n "$1" ] && host="$1" || usage
+host="falter-$(xxd -p -l 2 </dev/urandom)"
+variant="tunneldigger"
 
-vmdir="/tmp/falter/vm/$host"
+[ -n "$1" ] && version="$1" || usage "$variant" >&2
+
+vmdir="tmp/vm/$host"
 mkdir -p "$vmdir"
 
-# to run it in container: cd /vmdir/builter-test ; ./wizard-tester.py node_example.json
-cp -avx "$(realpath "$(dirname "$0")")" "$vmdir/builter-test"
+cp "out/$version/$variant"/x86/64/openwrt-*-generic-kernel.bin "$vmdir/kernel.bin"
+test/extract-vmlinux.sh "$vmdir/kernel.bin" >"$vmdir/vmlinux"
 
-wget -nv -O "$vmdir/extract-vmlinux.sh" https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-vmlinux
-chmod +x "$vmdir/extract-vmlinux.sh"
-
-wget -nv -O "$vmdir/kernel.bin" https://firmware.berlin.freifunk.net/unstable/snapshot/tunneldigger/x86/64/openwrt-freifunk-falter-snapshot-x86-64-generic-kernel.bin
-# cp firmwares/tunneldigger/x86/64/openwrt-freifunk-falter-snapshot-x86-64-generic-kernel.bin "$vmdir/kernel.bin"
-"$vmdir"/extract-vmlinux.sh "$vmdir/kernel.bin" > "$vmdir/vmlinux"
-
-wget -nv -O "$vmdir/rootfs.img.gz" https://firmware.berlin.freifunk.net/unstable/snapshot/tunneldigger/x86/64/openwrt-freifunk-falter-snapshot-x86-64-generic-ext4-rootfs.img.gz
-# cp firmwares/tunneldigger/x86/64/openwrt-freifunk-falter-snapshot-x86-64-generic-ext4-rootfs.img.gz "$vmdir/rootfs.img.gz"
-gunzip -c "$vmdir/rootfs.img.gz" > "$vmdir/rootfs.img"
+cp "out/$version/$variant"/x86/64/openwrt-*-generic-ext4-rootfs.img.gz "$vmdir/rootfs.img.gz"
+gunzip -c "$vmdir/rootfs.img.gz" >"$vmdir/rootfs.img"
 
 # heredoc without variable interpolation
-cat << 'EOF' > "$vmdir/vmconfig.json"
+cat <<'EOF' >"$vmdir/vmconfig.json"
 {
   "machine-config": {
     "vcpu_count": 1,
     "mem_size_mib": 128,
-    "smt": false
+    "smt": true
   },
   "boot-source": {
     "kernel_image_path": "./vmlinux",
@@ -67,13 +76,14 @@ cat << 'EOF' > "$vmdir/vmconfig.json"
 EOF
 
 # heredoc with variable interpolation
-cat << EOF > "$vmdir/entrypoint.sh"
-#!/bin/sh
+cat <<EOF >"$vmdir/entrypoint.sh"
+#!/bin/bash
 
 ip tuntap add dev vmeth0 mode tap
 ip link set up vmeth0
-ip addr add 192.168.42.102/24 dev vmeth0
+# ip addr add 192.168.42.102/24 dev vmeth0
 
+# vm's wan interface
 ip tuntap add dev vmeth1 mode tap
 ip link set up vmeth1
 
@@ -86,13 +96,10 @@ ip route del \`ip r | grep 'default'\`
 ip route add 10.0.2.0/24 dev wan
 ip route add default via 10.0.2.2 dev wan
 
-socat -d TCP-LISTEN:22,fork,reuseaddr TCP-CONNECT:192.168.42.1:22 &
-socat -d TCP-LISTEN:80,fork,reuseaddr TCP-CONNECT:192.168.42.1:80 &
-socat -d TCP-LISTEN:443,fork,reuseaddr TCP-CONNECT:192.168.42.1:443 &
-
 cd /vmdir
 
 while true ; do
+  /vmdir/portfwd.sh vmeth0 &
   firecracker --no-api --no-seccomp --config-file vmconfig.json
   echo "Restarting VM in 5 seconds..."
   sleep 5
@@ -100,22 +107,78 @@ done
 EOF
 chmod +x "$vmdir/entrypoint.sh"
 
-podman build -t localhost/falter-testing -f - << EOF
+# heredoc without variable interpolation
+cat <<'EOF' >"$vmdir/portfwd.sh"
+#!/bin/bash
+
+ifname="$1"
+
+ip addr flush $ifname
+killall dhclient || true
+killall socat || true
+
+# heredoc with variable interpolation
+cat << EOF2 > /etc/dhcp/dhclient.conf
+interface "$ifname" {
+  request subnet-mask, broadcast-address, interface-mtu;
+  initial-interval 1;
+  backoff-cutoff 2;
+}
+EOF2
+
+mkdir -p /run/dhcp
+dhclient -d $ifname &
+
+netmask() {
+  local mask=$((0xffffffff << (32 - $1))); shift
+  local ip
+  for _ in 1 2 3 4; do
+    ip=$((mask & 0xff))${ip:+.}$ip
+    mask=$((mask >> 8))
+  done
+  echo "$ip"
+}
+
+nth_ip() {
+  IFS=". /" read -r i1 i2 i3 i4 mask <<< "$1"
+  IFS=" ." read -r m1 m2 m3 m4 <<< "$(netmask "$mask")"
+  printf "%d.%d.%d.%d\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$(($2 + (i4 & m4)))"
+}
+
+while true; do
+  sleep 1
+  localip=$(ip -j a s $ifname | jq -r '.[].addr_info[] | select(.family == "inet") | [.local,.prefixlen|tostring] | join("/")')
+  [ -z "$localip" ] || break
+done
+
+gw="$(nth_ip "$localip" 1)"
+echo "portfwd.sh: router on $ifname is probably $gw"
+
+socat -d -4 TCP-LISTEN:8022,fork,reuseaddr TCP-CONNECT:$gw:22 &
+socat -d -4 UDP-LISTEN:8053,fork,reuseaddr UDP-CONNECT:$gw:53 &
+socat -d -4 TCP-LISTEN:8080,fork,reuseaddr TCP-CONNECT:$gw:80 &
+socat -d -4 TCP-LISTEN:8443,fork,reuseaddr TCP-CONNECT:$gw:443 &
+echo "portfwd.sh: available on 127.0.0.1: 8022 (ssh) 8053 (dns) 8080 (http) 8443 (https)"
+EOF
+chmod +x "$vmdir/portfwd.sh"
+
+# heredoc without variable interpolation
+podman build -t localhost/falter-testing -f - <<'EOF'
 FROM alpine:edge
 
 RUN echo https://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
 RUN apk upgrade musl
-RUN apk add bash openssh-client git vim mtr curl wget tcpdump iproute2 bridge-utils dhclient firecracker socat
+RUN apk add bash openssh-client git vim mtr curl wget tcpdump iproute2 bridge-utils dhclient firecracker socat jq
 
 RUN apk add python3 py3-pip ca-certificates py3-openssl openssl-dev
 RUN pip3 install --break-system-packages selenium pyvirtualdisplay
-
 RUN apk add firefox-esr xvfb dbus-x11 ttf-freefont
 RUN ln -s /usr/bin/firefox-esr /usr/bin/firefox
 EOF
 
-podman run -it --rm --name="$host" -v "$vmdir:/vmdir:Z" --user=root --userns=keep-id --device=/dev/kvm --device=/dev/net/tun --security-opt="label=disable" --cap-add=NET_ADMIN --cap-add=NET_RAW --network=slirp4netns:mtu=1500 -p 8022:22 -p 8080:80 -p 8443:443 localhost/falter-testing /vmdir/entrypoint.sh
+# to run tests in container: cd /vmdir/builter-test ; ./wizard-tester.py node_example.json
+cp -avx "test" "$vmdir/builter-test"
 
-# podman run -t --rm --name="$host" -v "$vmdir:/vmdir:Z" --user=root --userns=keep-id --device=/dev/kvm --device=/dev/net/tun --security-opt="label=disable" --cap-add=NET_ADMIN --cap-add=NET_RAW --network=pasta:-a,10.0.2.0,-n,24,-g,10.0.2.2,--dns-forward,10.0.2.3,-m,1500 docker.io/library/alpine:3.18 /vmdir/entrypoint.sh
+podman run -it --rm --name="$host" -v "$(realpath $vmdir):/vmdir:Z" --user=root --userns=keep-id --device=/dev/kvm --device=/dev/net/tun --security-opt="label=disable" --cap-add=NET_ADMIN --cap-add=NET_RAW --network=slirp4netns:mtu=1280 -p 8022:8022 -p 8053:8053/udp -p 8080:8080 -p 8443:8443 localhost/falter-testing /vmdir/entrypoint.sh
 
 echo "Done."
