e7ceb56ce90fd10d27840044a8286595cf9fe2c3
Packet Please <pktpls@systemli.org>
2024-06-02 20:30:58 +0200

    build: remove old build_falter script

--- scripts/02-favicon.sh
+++
@@ -1,8 +0,0 @@
-# shellcheck shell=bash
-
-# get current path of script. Thus we can call the script from everywhere.
-SCRIPTPATH=$(dirname "$(readlink -f "$0")")
-
-mkdir -p "$SCRIPTPATH/../embedded-files/www/luci-static/bootstrap" || exit 42
-cp "$SCRIPTPATH/../store/favicon.png" "$SCRIPTPATH/../embedded-files/www/luci-static/bootstrap" || exit 42
-
