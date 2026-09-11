72b988b3ec351c401ed5682f37a723d05b5024db
Packet Please <pktpls@systemli.org>
2026-09-11 14:42:28 +0200

    build: pin 25.12 and 24.12 to their latest releases

--- build/build.sh
+++ build/build.sh
@@ -81,8 +81,8 @@ rootdir=$(pwd)
 # map falter's versioning to openwrt's release branches
 orelease="snapshot"
 frelease="snapshot"
-[[ "$fversion" =~ ^1\.6\. ]] && orelease="25.12-SNAPSHOT" && frelease="1.6.0-snapshot"
-[[ "$fversion" =~ ^1\.5\. ]] && orelease="24.10-SNAPSHOT" && frelease="1.5.0-snapshot"
+[[ "$fversion" =~ ^1\.6\. ]] && orelease="25.12.5" && frelease="1.6.0-snapshot"
+[[ "$fversion" =~ ^1\.5\. ]] && orelease="24.10.8" && frelease="1.5.0-snapshot"
 [[ "$fversion" =~ ^1\.4\. ]] && orelease="23.05.6" && frelease="1.4.0-snapshot"
 [[ "$fversion" =~ ^1\.3\. ]] && orelease="22.03.7" && frelease="1.3.0-snapshot"
 [[ "$fversion" =~ ^1\.2\. ]] && orelease="21.02.7" && frelease="1.2.3-snapshot"
